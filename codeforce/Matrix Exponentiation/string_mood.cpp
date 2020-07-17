92b35c250ad2f652a74bce6568533e89c0fd8e86
deveshkau <deveshkau@gmail.com>
2020-07-17 20:01:40 +0530

    methodology of matrix exponentiation

--- codeforce/Matrix Exponentiation/string_mood.cpp
+++ codeforce/Matrix Exponentiation/string_mood.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define mod 1000000007
 using namespace std;
 
 vector<vector<int>> multiply(vector<vector<int>> a, vector<vector<int>> b)
@@ -8,7 +9,7 @@ vector<vector<int>> multiply(vector<vector<int>> a, vector<vector<int>> b)
     for (int i=0;i<2;i++)
         for (int j=0; j<2 ; j++)
             for (int k=0; k<2 ; k++)
-                product[i][k] += a[i][j] * b[j][k];
+                product[i][k] += (a[i][j] * b[j][k]) % mod;
     
     return product;
 }
@@ -37,3 +38,28 @@ int main()
     cout<<result[0][0];
     return 0;
 }
+
+
+/*
+    Solution 1
+    int happy = 1;
+    int sad = 0;
+    for (int i = 0; i < n; i++) {
+        int new_happy = 19 * happy + 6 * sad;
+        int new_sad = 7 * happy + 20 * sad;
+        happy = new_happy;
+        sad = new_sad;
+    }
+
+    Solution 2
+
+    int dp[2] = {1,0};
+    for (int i = 0; i < n; i++) {
+        int new_dp[2] = {0, 0};
+        new_dp[0] += dp[0] * 19;
+        new_dp[1] += dp[0] * 7;
+        new_dp[0] += dp[1]  * 6;
+        new_dp[1] += dp[1] * 20;
+        dp = new_dp;        //  new_dp[j] += dp[i] + m[i][j]
+    }
+*/
