ce6170df2fc16ac53f2b57fb0893bcdd67ec051b
deveshkau <deveshkau@gmail.com>
2020-07-20 21:14:39 +0530

    breadboard

--- codeforce/Breadboard_Capacity.cpp
+++ codeforce/Breadboard_Capacity.cpp
@@ -30,19 +30,41 @@ const ll   mod   =  998244353;
 const ll   INF   =  0x7f7f7f7f7f7f7f7f;
 const int  INFi  =  0x7f7f7f7f;
  
-int test = 1, n, a[N], b[N], ct[N], c1[N], c2[N], vis[N];
-vll res;
+int test = 1;
 
-void solve(){
-	res.clear();
-	cin>>n;
-	
+//const int N=101000;
+
+int pb[N],pr[N],n,m,q,dp[N][2];
+char L[N],R[N],U[N],D[N];
+int solve(char *L,char *R,char *U,char *D,int n,int m,char r,char b) {
+	dp[0][0]=dp[0][1]=0;
+	rep(i,0,n) {
+		dp[0][0]+=(L[i]==b);
+		dp[0][1]+=(L[i]==r);
+	}
+	dp[0][0]+=(U[0]==b)+(D[0]==b);
+	dp[0][1]+=(U[0]==r)+(D[0]==r);
+	rep(i,1,m) {
+		dp[i][0]=min(dp[i-1][0],dp[i-1][1]+n)+(U[i]==b)+(D[i]==b);
+		dp[i][1]=min(dp[i-1][1],dp[i-1][0]+n)+(U[i]==r)+(D[i]==r);
+	}
+	rep(i,0,n) {
+		dp[m-1][0]+=(R[i]==b);
+		dp[m-1][1]+=(R[i]==r);
+	}
+	return min(dp[m-1][0],dp[m-1][1]);
 }
  
 int main(){
 	GODSPEED;
-	cin>>test;
-	for(int i = 1; i <= test; i++){
-		solve();
-	}
+	cin>>n>>m>>q;
+	cin>>L>>R>>U>>D;
+	// scanf("%s%s%s%s",L,R,U,D);
+	int pR=0;
+	rep(i,0,n) pR+=(L[i]=='R')+(R[i]=='R');
+	rep(i,0,m) pR+=(U[i]=='R')+(D[i]=='R');
+	int ans = min(pR,2*(n+m)-pR);
+	ans = min(ans, solve(L,R,U,D,n,m,'R','B'));
+	ans = min(ans, solve(U,D,L,R,m,n,'R','B'));
+	cout<<ans<<endl;
 }
