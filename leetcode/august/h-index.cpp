a912ecb34a85acb39b2a846360a83063f9cf32f5
deveshkau <deveshkau@gmail.com>
2020-08-11 22:28:14 +0530

    lc aug day 11

--- leetcode/august/h-index.cpp
+++ leetcode/august/h-index.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         sort(citations.begin(),citations.end());
-        int sz = citations.size();
-        int md = (sz+1)/2;
-        int mid = citations[md];
-        if(mid == md) return mid;
-        int index = md;
-        while(mid != md && index>0){
-            mid = citations[--index];
-            if(mid == md) return mid;
+        int h=0,index=citations.size()-1;
+        while(index>-1){
+            if(citations[index]>h) {
+                h++;
+                index--;
+            } else {
+                return h;
+            }
         }
-        return 0;
+        return h;
     }
 };
