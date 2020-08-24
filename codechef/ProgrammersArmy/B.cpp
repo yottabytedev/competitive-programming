c71b8ecadeac99c18edcf84f50f3bcaf783ef890
deveshkau <deveshkau@gmail.com>
2020-08-24 20:38:32 +0530

    programmers army hiring problem b

---
+++ codechef/ProgrammersArmy/B.cpp
@@ -0,0 +1,474 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define  GODSPEED       ios:: sync_with_stdio(0);cin.tie(0);cout.tie(0);cout<<fixed;cout<<setprecision(15);
+#define  f              first                 
+#define  s              second
+#define  endl           "\n"
+#define  pb             push_back  
+#define  mset(a,x)      memset(a,x,sizeof(a))
+#define  debv(a)        for(auto it: a)cout<<it<<" ";cout<<endl;
+#define  deb1(a)        cout<<a<<endl;
+#define  deb2(a,b)      cout<<a<<" "<<b<<endl;
+#define  deb3(a,b,c)    cout<<a<<" "<<b<<" "<<c<<endl;
+#define  deb4(a,b,c,d)  cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;            
+#define  For(i,n)       for(int i=0;i<n;i++)
+#define  Fori(i,n)      for(int i=n;i>=0;i--)
+
+template<typename T, bool maximum_mode = false>
+struct RMQ {
+    int n = 0;
+    vector<T> values;
+    vector<vector<int>> range_low;
+ 
+    RMQ(const vector<T> &_values = {}) {
+        if (!_values.empty())
+            build(_values);
+    }
+ 
+    static int largest_bit(int x) {
+        return 31 - __builtin_clz(x);
+    }
+ 
+    // Note: when `values[a] == values[b]`, returns b.
+    int better_index(int a, int b) const {
+        return (maximum_mode ? values[b] < values[a] : values[a] < values[b]) ? a : b;
+    }
+ 
+    void build(const vector<T> &_values) {
+        values = _values;
+        n = int(values.size());
+        int levels = largest_bit(n) + 1;
+        range_low.resize(levels);
+ 
+        for (int k = 0; k < levels; k++)
+            range_low[k].resize(n - (1 << k) + 1);
+ 
+        for (int i = 0; i < n; i++)
+            range_low[0][i] = i;
+ 
+        for (int k = 1; k < levels; k++)
+            for (int i = 0; i <= n - (1 << k); i++)
+                range_low[k][i] = better_index(range_low[k - 1][i], range_low[k - 1][i + (1 << (k - 1))]);
+    }
+ 
+    // Note: breaks ties by choosing the largest index.
+    int query_index(int a, int b) const {
+        assert(0 <= a && a < b && b <= n);
+        int level = largest_bit(b - a);
+        return better_index(range_low[level][a], range_low[level][b - (1 << level)]);
+    }
+ 
+    T query_value(int a, int b) const {
+        return values[query_index(a, b)];
+    }
+};
+
+struct LCA {
+    int n = 0;
+    vector<vector<int>> adj;
+    vector<int> parent, depth, subtree_size;
+    vector<int> euler, first_occurrence;
+    vector<int> tour_start, tour_end, postorder;
+    vector<int> tour_list, rev_tour_list;
+    vector<int> heavy_root;
+    RMQ<int> rmq;
+    bool built;
+ 
+    LCA(int _n = 0) {
+        init(_n);
+    }
+ 
+    // Warning: this does not call build().
+    LCA(const vector<vector<int>> &_adj) {
+        init(_adj);
+    }
+ 
+    void init(int _n) {
+        n = _n;
+        adj.assign(n, {});
+        parent.resize(n);
+        depth.resize(n);
+        subtree_size.resize(n);
+        first_occurrence.resize(n);
+        tour_start.resize(n);
+        tour_end.resize(n);
+        postorder.resize(n);
+        tour_list.resize(n);
+        heavy_root.resize(n);
+        built = false;
+    }
+ 
+    // Warning: this does not call build().
+    void init(const vector<vector<int>> &_adj) {
+        init(int(_adj.size()));
+        adj = _adj;
+    }
+ 
+    void add_edge(int a, int b) {
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+ 
+    int degree(int v) const {
+        return int(adj[v].size()) + (built && parent[v] >= 0);
+    }
+ 
+    void dfs(int node, int par) {
+        parent[node] = par;
+        depth[node] = par < 0 ? 0 : depth[par] + 1;
+        subtree_size[node] = 1;
+ 
+        // Erase the edge to parent.
+        adj[node].erase(remove(adj[node].begin(), adj[node].end(), par), adj[node].end());
+ 
+        for (int child : adj[node]) {
+            dfs(child, node);
+            subtree_size[node] += subtree_size[child];
+        }
+ 
+        // Heavy-light subtree reordering.
+        sort(adj[node].begin(), adj[node].end(), [&](int a, int b) {
+            return subtree_size[a] > subtree_size[b];
+        });
+    }
+ 
+    int tour, post_tour;
+ 
+    void tour_dfs(int node, bool heavy) {
+        heavy_root[node] = heavy ? heavy_root[parent[node]] : node;
+        first_occurrence[node] = int(euler.size());
+        euler.push_back(node);
+        tour_list[tour] = node;
+        tour_start[node] = tour++;
+        bool heavy_child = true;
+ 
+        for (int child : adj[node]) {
+            tour_dfs(child, heavy_child);
+            euler.push_back(node);
+            heavy_child = false;
+        }
+ 
+        tour_end[node] = tour;
+        postorder[node] = post_tour++;
+    }
+ 
+    void build(int root = -1) {
+        parent.assign(n, -1);
+ 
+        if (0 <= root && root < n)
+            dfs(root, -1);
+ 
+        for (int i = 0; i < n; i++)
+            if (i != root && parent[i] < 0)
+                dfs(i, -1);
+ 
+        tour = post_tour = 0;
+        euler.clear();
+        euler.reserve(2 * n);
+ 
+        for (int i = 0; i < n; i++)
+            if (parent[i] < 0) {
+                tour_dfs(i, false);
+                // Add a -1 in between connected components to help us detect when nodes aren't connected.
+                euler.push_back(-1);
+            }
+ 
+        rev_tour_list = tour_list;
+        reverse(rev_tour_list.begin(), rev_tour_list.end());
+        assert(int(euler.size()) == 2 * n);
+        vector<int> euler_depths;
+        euler_depths.reserve(euler.size());
+ 
+        for (int node : euler)
+            euler_depths.push_back(node < 0 ? node : depth[node]);
+ 
+        rmq.build(euler_depths);
+        built = true;
+    }
+ 
+    pair<int, int> find_farthest(int node, int par, int path) const {
+        pair<int, int> current = {path, node};
+ 
+        for (int neighbor : adj[node])
+            if (neighbor != par)
+                current = max(current, find_farthest(neighbor, node, path + 1));
+ 
+        return current;
+    }
+ 
+    // Warning: this must be called before build(), since build() erases half of the edges.
+    pair<int, pair<int, int>> get_diameter() const {
+        int u = find_farthest(0, -1, 0).second;
+        pair<int, int> farthest = find_farthest(u, -1, 0);
+        int v = farthest.second;
+        return {farthest.first, {u, v}};
+    }
+ 
+    // Note: returns -1 if `a` and `b` aren't connected.
+    int get_lca(int a, int b) const {
+        a = first_occurrence[a];
+        b = first_occurrence[b];
+ 
+        if (a > b)
+            swap(a, b);
+ 
+        return euler[rmq.query_index(a, b + 1)];
+    }
+ 
+    bool is_ancestor(int a, int b) const {
+        return tour_start[a] <= tour_start[b] && tour_start[b] < tour_end[a];
+    }
+ 
+    bool on_path(int x, int a, int b) const {
+        return (is_ancestor(x, a) || is_ancestor(x, b)) && is_ancestor(get_lca(a, b), x);
+    }
+ 
+    int get_dist(int a, int b) const {
+        return depth[a] + depth[b] - 2 * depth[get_lca(a, b)];
+    }
+ 
+    // Returns the child of `a` that is an ancestor of `b`. Assumes `a` is a strict ancestor of `b`.
+    int child_ancestor(int a, int b) const {
+        assert(a != b);
+        assert(is_ancestor(a, b));
+ 
+        // Note: this depends on RMQ breaking ties by latest index.
+        int child = euler[rmq.query_index(first_occurrence[a], first_occurrence[b] + 1) + 1];
+        assert(parent[child] == a);
+        assert(is_ancestor(child, b));
+        return child;
+    }
+ 
+    int get_kth_ancestor(int a, int k) const {
+        while (a >= 0) {
+            int root = heavy_root[a];
+ 
+            if (depth[root] <= depth[a] - k)
+                return tour_list[tour_start[a] - k];
+ 
+            k -= depth[a] - depth[root] + 1;
+            a = parent[root];
+        }
+ 
+        return a;
+    }
+ 
+    int get_kth_node_on_path(int a, int b, int k) const {
+        int anc = get_lca(a, b);
+        int first_half = depth[a] - depth[anc];
+        int second_half = depth[b] - depth[anc];
+        assert(0 <= k && k <= first_half + second_half);
+ 
+        if (k < first_half)
+            return get_kth_ancestor(a, k);
+        else
+            return get_kth_ancestor(b, first_half + second_half - k);
+    }
+ 
+    // Note: this is the LCA of any two nodes out of three when the third node is the root.
+    // It is also the node with the minimum sum of distances to all three nodes.
+    int get_common_node(int a, int b, int c) const {
+        // Return the deepest node among lca(a, b), lca(b, c), and lca(c, a).
+        int x = get_lca(a, b);
+        int y = get_lca(b, c);
+        int z = get_lca(c, a);
+ 
+        if (depth[y] > depth[x])
+            x = y;
+ 
+        if (depth[z] > depth[x])
+            x = z;
+ 
+        return x;
+    }
+ 
+    // Given a subset of k tree nodes, computes the minimal subtree that contains all the nodes (at most 2k - 1 nodes).
+    // Returns a list of {node, parent} for every node in the subtree. Runs in O(k log k).
+    vector<pair<int, int>> compress_tree(vector<int> nodes) const {
+        if (nodes.empty())
+            return {};
+ 
+        auto &&compare_tour = [&](int a, int b) { return tour_start[a] < tour_start[b]; };
+        sort(nodes.begin(), nodes.end(), compare_tour);
+        int k = int(nodes.size());
+ 
+        for (int i = 0; i < k - 1; i++)
+            nodes.push_back(get_lca(nodes[i], nodes[i + 1]));
+ 
+        sort(nodes.begin() + k, nodes.end(), compare_tour);
+        inplace_merge(nodes.begin(), nodes.begin() + k, nodes.end(), compare_tour);
+        nodes.erase(unique(nodes.begin(), nodes.end()), nodes.end());
+        vector<pair<int, int>> result = {{nodes[0], -1}};
+ 
+        for (int i = 1; i < int(nodes.size()); i++)
+            result.emplace_back(nodes[i], get_lca(nodes[i], nodes[i - 1]));
+ 
+        return result;
+    }
+};
+  
+// Given a date, returns number of days elapsed 
+// from the  beginning of the current year (1st 
+// jan). 
+int offsetDays(int d, int m, int y) 
+{ 
+    int offset = d; 
+  
+    switch (m - 1) 
+    { 
+    case 11: 
+        offset += 30; 
+    case 10: 
+        offset += 31; 
+    case 9: 
+        offset += 30; 
+    case 8: 
+        offset += 31; 
+    case 7: 
+        offset += 31; 
+    case 6: 
+        offset += 30; 
+    case 5: 
+        offset += 31; 
+    case 4: 
+        offset += 30; 
+    case 3: 
+        offset += 31; 
+    case 2: 
+        offset += 28; 
+    case 1: 
+        offset += 31; 
+    } 
+  
+    if (m > 2) 
+        offset += 1; 
+  
+    return offset; 
+} 
+  
+// Given a year and days elapsed in it, finds 
+// date by storing results in d and m. 
+void revoffsetDays(int offset, int y, int *d, int *m) 
+{ 
+    int month[13] = { 0, 31, 29, 31, 30, 31, 30, 
+                      31, 31, 30, 31, 30, 31 }; 
+  
+  
+    int i; 
+    for (i = 1; i <= 12; i++) 
+    { 
+        if (offset <= month[i]) 
+            break; 
+        offset = offset - month[i]; 
+    } 
+  
+    *d = offset; 
+    *m = i; 
+} 
+map<string, int> MONTHS
+    {
+        { "january", 1 },
+        { "february", 2 },
+        { "march", 3 },
+        { "april", 4 },
+        { "may", 5 },
+        { "june", 6 },
+        { "july", 7 },
+        { "august", 8 },
+        { "september", 9 },
+        { "october", 10 },
+        { "november", 11 },
+        { "december", 12 }
+    };
+
+map<int, string> rMONTHS
+    {
+        { 1,"january"},
+        { 2,"february"},
+        { 3,"march"},
+        { 4,"april"},
+        { 5,"may"},
+        { 6,"june" },
+        { 7,"july"},
+        { 8,"august"},
+        { 9,"september"},
+        { 10,"october"},
+        { 11,"november"},
+        { 12,"december"}
+    };
+  
+// Add x days to the given date. 
+void addDays(int d1, int m1, int y1, int x) 
+{ 
+    int offset1 = offsetDays(d1, m1, y1); 
+    int remDays = (366-offset1); 
+  
+    // y2 is going to store result year and 
+    // offset2 is going to store offset days 
+    // in result year. 
+    int y2, offset2; 
+    if (x <= remDays) 
+    { 
+        y2 = y1; 
+        offset2 = offset1 + x; 
+    } 
+  
+    else
+    { 
+        // x may store thousands of days. 
+        // We find correct year and offset 
+        // in the year. 
+        x -= remDays; 
+        y2 = y1 + 1; 
+        int y2days = 366; 
+        while (x >= y2days) 
+        { 
+            x -= y2days; 
+            y2++; 
+            y2days = 366; 
+        } 
+        offset2 = x; 
+    } 
+  
+    // Find values of day and month from 
+    // offset of result year. 
+    int m2, d2; 
+    revoffsetDays(offset2, y2, &d2, &m2); 
+  
+    deb2(d2,rMONTHS[m2])
+    // cout << "d2 = " << d2 << ", m2 = " << m2 
+    //      << ", y2 = " << y2; 
+}
+ 
+typedef  long long             ll;
+typedef  long double           ld;
+typedef  pair<ll,ll>           pll;
+typedef  vector<ll>            vll;
+typedef  unordered_map<ll,ll>  um;
+typedef  vector<pll>           vpll;
+ 
+const ll   N     =  1e6+5;
+const ll   mod   =  998244353;
+const ll   INF   =  0x7f7f7f7f7f7f7f7f;
+const int  INFi  =  0x7f7f7f7f;
+ 
+int test = 1, n, a[N], b[N], ct[N], c1[N], c2[N], vis[N];
+vll res;
+
+void solve(){
+	int d;
+    string month;
+
+    cin>>d>>month;
+    addDays(d,MONTHS[month],2020,183);
+}
+ 
+int main(){
+	GODSPEED;
+	cin>>test;
+	for(int i = 1; i <= test; i++){
+		solve();
+	}
+}
